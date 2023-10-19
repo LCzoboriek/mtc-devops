17ce17c94a7928191c13bb480092adfed708e3a3
Luke Czoboriek <lc169782@protonmail.com>
2023-10-19 16:10:18 +0100

    added in random

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+resource "random_id" "random" {
+  byte_length = 2
+
+}
+
 resource "aws_vpc" "ldc_vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support = true
   tags = {
-    Name = "ldc_vpc"
+    Name = "ldc_vpc-${random_id.random.dec}"
   }
 }
 
@@ -11,7 +16,7 @@ resource "aws_internet_gateway" "ldc_gw" {
   vpc_id = aws_vpc.ldc_vpc.id
 
   tags = {
-    Name = "ldc_igw"
+    Name = "ldc_igw-${random_id.random.dec}"
   }
 
 }
