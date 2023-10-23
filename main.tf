deb2fc55915bde9bc0e9f9a48b3400ca489e9d1b
Luke Czoboriek <luke.czoboriek@engineering.digital.dwp.gov.uk>
2023-10-23 11:26:34 +0100

    added in public subnet

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "random_id" "random" {
 }
 
 resource "aws_vpc" "ldc_vpc" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
     Name = "ldc_vpc-${random_id.random.dec}"
   }
@@ -25,16 +25,16 @@ resource "aws_internet_gateway" "ldc_gw" {
 }
 
 resource "aws_route_table" "ldc_public_rt" {
-   vpc_id = aws_vpc.ldc_vpc.id
-   tags = {
+  vpc_id = aws_vpc.ldc_vpc.id
+  tags = {
     Name = "ldc-public"
-   }
+  }
 }
 
 resource "aws_route" "default_route" {
-  route_table_id = aws_route_table.ldc_public_rt.id
+  route_table_id         = aws_route_table.ldc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.ldc_gw.id
+  gateway_id             = aws_internet_gateway.ldc_gw.id
 
 }
 
@@ -51,4 +51,16 @@ data "aws_availability_zones" "available" {
 } // The reason why we have this added, is because availability zones can change depending on the day and time
 // and when we specify the availbility zone in a subnet resource, it could encounter an issue
 // so we need to make sure this is dynamically assigned
+// like for example using data.aws_vailabilty_zones.available.names[0] will use the first zone available
+
+resource "aws_subnet" "ldc_public_subnet" {
+  vpc_id                            = aws_vpc.ldc_vpc.id
+  cidr_block                        = var.public_cidrs
+  map_public_ip_on_launch = true
+  availability_zone                 = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "ldc_public"
+  }
+}
 
