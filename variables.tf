edfe4b89e0d8036d723196391f71fe0dc563c08c
Luke Czoboriek <luke.czoboriek@engineering.digital.dwp.gov.uk>
2023-10-23 13:23:03 +0100

    removed variable dec

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "vpc_cidr" {
   type    = string
-  default = var.vpc_cidr
+  
 }
 
 variable "public_cidrs" {
   type    = list(string)
-  default = var.public_cidrs
+  
 }
 
 variable "private_cidrs" {
   type    = list(string)
-  default = var.private_cidrs
+  
 }
